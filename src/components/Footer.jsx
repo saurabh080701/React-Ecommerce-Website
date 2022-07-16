a5b3b36c4ab8a3fdd8862b44997cc5c3c13efcb2
saurabh080701 <raawatsaurabh885@gmail.com>
2022-07-16 09:48:46 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,142 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  display: flex;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ display: "none" })}
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
+
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>Saurabh & Vishal</Logo>
+        <Desc>
+        Check out all the freshest styles your closet needs in our men's clothing range. You'll find a roundup of everyday essentials, including tops and T-Shirts, as well as comfy lounge sets and underwear. Formal event coming up? Scroll no further than our men's blazers and suits for the sharpest looks and nail the dress code. When it comes to men's pants, there's chinos, joggers and cargo styles in all the staple colors. dreaming of denim? our men's jeans offer a range of fits to suit your style.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Man Grooming</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{marginRight:"10px"}}/> Lovely Professional University is a private university, Phagwara, Punjab, India. 
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{marginRight:"10px"}}/> +91 9214696942
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{marginRight:"10px"}} /> rawatsaurabh885@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.pinimg.com/564x/50/ab/67/50ab6748e47a2e88843997622f2391a3.jpg",
+      title: "MEN'S SUMMER COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "a6aab3",
+    },
+    {
+      id: 2,
+      img: "https://i.pinimg.com/564x/68/16/a3/6816a3d3f8ff2b00e12f88d3842e97fd.jpg",
+      title: "MEN'S AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "edece8",
+    },
+    {
+      id: 3,
+      img: "https://i.insider.com/5afdc7511ae6622b008b473b?width=800&format=jpeg&auto=webp",
+      title: "MEN'S GROOMING PRODUCTS",
+      desc: "Start Your Grooming Journey with us Up to 30% OFF ",
+      bg: "b0a594",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://img.dtcn.com/image/themanual/gp_200506_cutsclothing_summer_0566-800x800.jpg",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://static1.michael84.co.uk/wp-content/uploads/incase-bag-review-michael84-3-1080x720.jpg",
+      title: "LIGHT JACKETS",
+    },
+    {
+      id: 3,
+      img: "https://magees.ie/blog/wp-content/uploads/2017/06/wintergrooming.jpg",
+      title: "RUGGED, REFINED, RENEWED",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://media.gq.com/photos/5f297d3f280bb755b506527c/master/w_2000,h_1333,c_limit/Uniqlo-U-oversized-crew-neck-T-shirt.jpg",
+    },
+    {
+      id:2,
+      img:"https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/423553/item/goods_69_423553.jpg?width=1557&impolicy=quality_75",
+    },
+    {
+      id:3,
+      img:"https://www.ckf.hu/5540-large_default/zara-black-lining-leather-jacket.jpg",
+    },
+    {
+      id:4,
+      img:"https://www.mrandmrsstitch.com/images/gran-sasso-cashmere-crew-neck-knit-sweater-beige-p31724-317555_zoom.jpg",
+    },
+    {
+      id:5,
+      img:"https://imgmedia.lbb.in/media/2020/05/5ec91f255906a2060aaeeafb_1590239013599.jpg",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://pyxis.nymag.com/v1/imgs/5f4/29c/d8dde581ac53cff63d85191f435f41f4da-levis.rhorizontal.w600.jpg",
+    },
+    {
+      id:8,
+      img:"https://cdn.luxe.digital/media/2020/10/01123336/best-winter-coats-men-wool-mr-p-review-luxe-digital.jpg",
+    },
+  ]
